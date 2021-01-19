ef32ca2d467aa146efcbc06ed072a08fa7832544
Siddharth Patel <sidharthpatel012@gmail.com>
2021-01-19 04:11:05 -0500

    Generated a Navigation sidebar, dummy menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavbarComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidenavbar/sidenavbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavbar',
+  templateUrl: './sidenavbar.component.html',
+  styleUrls: ['./sidenavbar.component.css']
+})
+export class SidenavbarComponent implements OnInit {
+  navbarStatus=false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
