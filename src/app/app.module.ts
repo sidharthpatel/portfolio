93320b03921a72807a1a213e996d9642edfee75a
Siddharth Patel <sidharthpatel012@gmail.com>
2021-12-19 11:38:35 -0500

    Generated a clean navigation bar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,16 @@ import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
-import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavbarComponent,
-    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,6 +25,7 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
