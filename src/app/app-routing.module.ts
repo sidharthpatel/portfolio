f350807abe1e5cbd3b985d0244e67546f6acb27d
Siddharth Patel <sidharthpatel012@gmail.com>
2022-01-11 21:50:49 -0500

    Import bunch of mods to be used for the front end.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-    {path: 'home', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'projects', component: ProjectsComponent},
 ]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
