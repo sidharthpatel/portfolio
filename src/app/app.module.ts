59e6200c67a713489fa498973de9c44fc391657a
Siddharth Patel <sidharthpatel012@gmail.com>
2022-09-03 17:30:40 -0400

    Simple portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent,
     AppComponent,
     HomeComponent,
-    ExperienceComponent,
   ],
   imports: [
-    AppRoutingModule,
-    RouterModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    FormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
