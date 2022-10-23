5bc89837ee3881e161fd30faaaab31043ac7642b
Siddharth Patel <sidharthpatel012@gmail.com>
2022-10-23 10:10:18 -0400

    Add the app component to the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './home/home.component';
+import { ToggleSwitchComponent } from './toggle-switch/toggle-switch.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, HomeComponent, ToggleSwitchComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
