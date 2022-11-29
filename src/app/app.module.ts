1438c3cf3a2732a57c7dc5287f35ac292801e3fe
Siddharth Patel <sidharthpatel012@gmail.com>
2022-11-29 02:53:09 -0500

    Removed light theme and dark theme from app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ToggleSwitchComponent } from './toggle-switch/toggle-switch.component';
-import { LightThemeComponent } from './home/light-theme/light-theme.component';
-import { DarkThemeComponent } from './home/dark-theme/dark-theme.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ExperienceComponent } from './experience/experience.component';
 
@@ -14,8 +12,6 @@ import { ExperienceComponent } from './experience/experience.component';
     AppComponent,
     HomeComponent,
     ToggleSwitchComponent,
-    LightThemeComponent,
-    DarkThemeComponent,
     NavbarComponent,
     ExperienceComponent,
   ],
