9a891714f4b8bdcd19097bb5cbdd5bff3e21d942
Siddharth Patel <sidharthpatel012@gmail.com>
2025-01-01 17:37:45 -0500

    feat: center the skills header and sub header along with some margin.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { motion } from "framer-motion";
@@ -11,8 +10,12 @@ const Tech = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Tools that I use</p>
-        <h2 className={styles.sectionHeadText}>Tech Stack</h2>
+        <p className="sm:text-[18px] text-center text-[14px] text-secondary uppercase tracking-wider">
+          Tools that I use
+        </p>
+        <h2 className="text-white text-center mb-5 font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+          Tech Stack
+        </h2>
       </motion.div>
 
       <div className="flex flex-row flex-wrap justify-center gap-10">
