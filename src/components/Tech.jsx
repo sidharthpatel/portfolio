1acea7196cecd3fa7ca818e2c3b74cb413355734
Siddharth Patel <sidharthpatel012@gmail.com>
2025-01-01 16:44:41 -0500

    feat: apply styles to the skill section.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,18 +9,21 @@ import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Tools that I use</p>
         <h2 className={styles.sectionHeadText}>Tech Stack</h2>
       </motion.div>
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <p className="text-center text-secondary">{technology.name}</p>
-        </div>
-      ))}
-    </div>
+
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className="text-center text-secondary">{technology.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
