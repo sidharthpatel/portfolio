e4b6290cef2776a05e4803ba35ae64a870b92641
Siddharth Patel <sidharthpatel012@gmail.com>
2025-01-01 16:44:41 -0500

    feat: apply styles to the skill section.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
+import { motion } from "framer-motion";
 import { technologies } from "../constants";
 import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      <p className={styles.sectionSubText}>Tools that I use</p>
-      <h2 className={styles.sectionHeadText}>Tech Stack</h2>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Tools that I use</p>
+        <h2 className={styles.sectionHeadText}>Tech Stack</h2>
+      </motion.div>
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
