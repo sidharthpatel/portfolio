1c6f2d272f9f3d9dfdd87c33e4aa0704ac275b6e
Siddharth Patel <sidharthpatel012@gmail.com>
2025-01-01 16:33:10 -0500

    feat: add section text to the tech stack.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,18 +3,21 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
 
 const Tech = () => {
-	return (
-		<div className="flex flex-row flex-wrap justify-center gap-10">
-			{technologies.map((technology) => (
-				<div className="w-28 h-28" key={technology.name}>
-					<BallCanvas icon={technology.icon} />
-					<p className="text-center text-secondary">{technology.name}</p>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      <p className={styles.sectionSubText}>Tools that I use</p>
+      <h2 className={styles.sectionHeadText}>Tech Stack</h2>
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+          <p className="text-center text-secondary">{technology.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default SectionWrapper(Tech, "");
+export default SectionWrapper(Tech, "skills");
